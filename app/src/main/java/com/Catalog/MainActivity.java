766e7c6f0925a5fe899ab3d52cd6f08ea06ea999
Manpreet <sandhu.07@gamil.com>
2020-03-20 14:46:22 -0800

    adding second activity

--- app/src/main/java/com/Catalog/MainActivity.java
+++ app/src/main/java/com/Catalog/MainActivity.java
@@ -2,7 +2,9 @@ package com.Catalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void addEmployee(View view){
+        Intent intent = new Intent(this, RegisterEmployee.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/Catalog/RegisterEmployee.java
@@ -0,0 +1,14 @@
+package com.Catalog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterEmployee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_employee);
+    }
+}
