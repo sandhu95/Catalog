11ba3229673010a4abe71860e037ecc7295baf48
Manpreet <sandhu.07@gamil.com>
2020-03-30 13:13:50 -0800

    Added getters and setters to Employee class

--- app/src/main/java/com/Catalog/Employee.java
+++ app/src/main/java/com/Catalog/Employee.java
@@ -7,5 +7,35 @@ public class Employee {
         private int birthYear;
         private double monthlySalary;
         private int rate;
+        public String getName() {
+                return name;
+        }
+        public void setName(String name) {
+                this.name = name;
+        }
+        public int getAge() {
+                return age;
+        }
+        public void setAge(int age) {
+                this.age = age;
+        }
+        public int getBirthYear() {
+                return birthYear;
+        }
+        public void setBirthYear(int birthYear) {
+                this.birthYear = birthYear;
+        }
+        public double getMonthlySalary() {
+                return monthlySalary;
+        }
+        public void setMonthlySalary(double monthlySalary) {
+                this.monthlySalary = monthlySalary;
+        }
+        public int getRate() {
+                return rate;
+        }
+        public void setRate(int rate) {
+                this.rate = rate;
+        }
 
 }
