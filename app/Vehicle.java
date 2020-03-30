72f8c6bf7f4a2fc917ab3479a5741b0467189022
Manpreet <sandhu.07@gamil.com>
2020-03-30 12:47:48 -0800

    Making constructor, getter and setters of vehicle class

--- app/Vehicle.java
+++ app/Vehicle.java
@@ -3,4 +3,29 @@ public class Vehicle {
     private int model;
     private String plate;
     private String color;
+    public int getModel() {
+        return model;
+    }
+    public void setModel(int model) {
+        this.model = model;
+    }
+    public String getPlate() {
+        return plate;
+    }
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public Vehicle(int model, String plate, String color) {
+        super();
+        this.model = model;
+        this.plate = plate;
+        this.color = color;
+    }
+
 }
