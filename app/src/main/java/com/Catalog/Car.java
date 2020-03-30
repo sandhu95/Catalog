53b24ba23641869c9607077f0a71dbc0d3edc544
Ramanpreet Kaur <ramanpreetkaor@gmail.com>
2020-03-30 14:43:28 -0400

    adding new class Car that extends Vehicle parent class

---
+++ app/src/main/java/com/Catalog/Car.java
@@ -0,0 +1,12 @@
+package com.Catalog;
+
+public class Car extends Vehicle
+{
+    private String type;
+
+    public Car(int model, String plate, String color)
+    {
+        super(model, plate, color);
+
+    }
+}
