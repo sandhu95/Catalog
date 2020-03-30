74448c59dd407c2fb8efaa873373b637ce19af44
Manpreet <sandhu.07@gamil.com>
2020-03-30 13:12:19 -0800

    started Employee class

---
+++ app/src/main/java/com/Catalog/Employee.java
@@ -0,0 +1,11 @@
+package com.Catalog;
+
+public class Employee {
+
+        private String name;
+        private int age;
+        private int birthYear;
+        private double monthlySalary;
+        private int rate;
+
+}
