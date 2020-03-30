6b1c00a56b87a2f3d0693ab024145c48c542ee08
Parneet kaur Jattana <peryj94@gmail.com>
2020-03-30 17:25:21 -0400

    Adding Getters and Setters in Programmer Class

--- app/src/main/java/com/Catalog/Programmer.java
+++ app/src/main/java/com/Catalog/Programmer.java
@@ -1,5 +1,13 @@
 package com.Catalog;
 
 public class Programmer extends Employee {
+    public int getNbProjects() {
+        return nbProjects;
+    }
+
+    public void setNbProjects(int nbProjects) {
+        this.nbProjects = nbProjects;
+    }
+
     public int nbProjects;
 }
