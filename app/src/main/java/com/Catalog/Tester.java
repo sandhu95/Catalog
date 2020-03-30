d4db1d0e07900e8f67c1da8a5160f41e2048b15e
Parneet kaur Jattana <peryj94@gmail.com>
2020-03-30 17:25:55 -0400

    Adding Getters and Setters in Tester Class

--- app/src/main/java/com/Catalog/Tester.java
+++ app/src/main/java/com/Catalog/Tester.java
@@ -1,5 +1,13 @@
 package com.Catalog;
 
 public class Tester extends Employee {
+    public int getNbBugs() {
+        return nbBugs;
+    }
+
+    public void setNbBugs(int nbBugs) {
+        this.nbBugs = nbBugs;
+    }
+
     public int nbBugs;
 }
