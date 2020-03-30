932fec2df2906f188d2edc8ff555a582d27e0d6b
Ramanpreet Kaur <ramanpreetkaor@gmail.com>
2020-03-30 14:47:40 -0400

    adding getters and setters in Car Class

--- app/src/main/java/com/Catalog/Car.java
+++ app/src/main/java/com/Catalog/Car.java
@@ -3,6 +3,12 @@ package com.Catalog;
 public class Car extends Vehicle
 {
     private String type;
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public Car(int model, String plate, String color)
     {
