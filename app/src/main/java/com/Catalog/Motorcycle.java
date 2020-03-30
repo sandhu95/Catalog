acff4948c71fe99628247a84e6d47e57199bbd90
Ramanpreet Kaur <ramanpreetkaor@gmail.com>
2020-03-30 15:02:15 -0400

    Added getters and setters in Motorcycle class to access private field

--- app/src/main/java/com/Catalog/Motorcycle.java
+++ app/src/main/java/com/Catalog/Motorcycle.java
@@ -3,10 +3,15 @@ package com.Catalog;
 public class Motorcycle extends Vehicle
 {
     private String sideCar;
-
     public Motorcycle(int model, String plate, String color, String sideCar)
     {
         super(model, plate, color);
+        this.sideCar = sideCar;
+    }
+    public String getSideCar() {
+        return sideCar;
+    }
+    public void setSideCar(String sideCar) {
         this.sideCar = sideCar;
     }
 }
