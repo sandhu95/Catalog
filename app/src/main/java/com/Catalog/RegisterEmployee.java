955d3565a3b17003c282cac4935c54c285e83b2c
Ramanpreet Kaur <ramanpreetkaor@gmail.com>
2020-03-31 14:40:27 -0400

    Added variable and fetching the value from xml file by using findviewById

--- app/src/main/java/com/Catalog/RegisterEmployee.java
+++ app/src/main/java/com/Catalog/RegisterEmployee.java
@@ -3,12 +3,43 @@ package com.Catalog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class RegisterEmployee extends AppCompatActivity {
+
+    EditText firstName,lastName,birthYear,monthlySalary,occupationRate,employeeID,projects,clients,
+    bugs,vehicleModel,plateNumber;
+    Spinner  employeeType,carTypeSpinner,vehicleColorSpinner;
+    RadioButton  carRadioBtn,motorcycleRadioBtn,sidecarYesRadioBtn,sidecarNoRadioBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_employee);
+        firstName = findViewById(R.id.firstNameText);
+        lastName = findViewById(R.id.lastNameText);
+        birthYear= findViewById(R.id.birthYearText);
+        monthlySalary= findViewById(R.id.monthlySalaryText);
+        occupationRate  = findViewById(R.id.occupationRateText);
+        employeeID  = findViewById(R.id.employeeIDText);
+        employeeType = findViewById(R.id.employeeTypeSpinner);
+        projects = findViewById(R.id.projectsText);
+        clients = findViewById(R.id.clientsText);
+        bugs = findViewById(R.id.bugsText);
+        carRadioBtn = findViewById(R.id.carRadioButton);
+        motorcycleRadioBtn = findViewById(R.id.motorcycleRadioButton);
+        sidecarYesRadioBtn = findViewById(R.id.sidecarYesRadioButton);
+        sidecarNoRadioBtn = findViewById(R.id.sidecarNoRadioButton);
+        carTypeSpinner = findViewById(R.id.carTypeSpinner);
+        vehicleModel = findViewById(R.id.vehicleModelText);
+        plateNumber = findViewById(R.id.plateNumberText);
+        vehicleColorSpinner = findViewById(R.id.vehicleColorSpinner);
+
+
     }
+
+
 }
