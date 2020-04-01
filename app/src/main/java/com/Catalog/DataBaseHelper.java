44081a9857eeb4a3f37472555a30704ea587d351
Parneet kaur Jattana <peryj94@gmail.com>
2020-03-31 22:10:58 -0400

    Editing DatabaseHelper.java

--- app/src/main/java/com/Catalog/DataBaseHelper.java
+++ app/src/main/java/com/Catalog/DataBaseHelper.java
@@ -2,6 +2,7 @@ package com.Catalog;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.ColorSpace;
 
 import androidx.annotation.Nullable;
 
@@ -11,7 +12,7 @@ public  class DataBaseHelper extends SQLiteOpenHelper
 
         private static final String DATABASE_NAME = "EmployeeDatabase";
         private static final int DATABASE_VERSION = 1;
-        private static final String TABLE_NAME = "employees";
+        private static final String TABLE_NAME1 = "Employee";
         private static final String  EmployeeID = "";
         private static final String FIRST_NAME = "";
         private static final String LAST_NAME = "";
@@ -19,9 +20,20 @@ public  class DataBaseHelper extends SQLiteOpenHelper
         private static final String MONTHLY_SALARY = "";
         private static final String OCCUPATION_RATE  = "";
         private static final String EMPLOYEE_TYPE = "";
-        private static final String PROJECTS = "";
-        private static final String CLIENTS = "";
-        private static final String BUGS= "";
+        private static final String TABLE_NAME2 = "Vehicle";
+        private static final String model = "";
+        private static final String plate = "";
+        private static final String color = "";
+        private static final String TABLE_NAME3 = "Car";
+        private static final String type = "";
+        private static final String TABLE_NAME4 = "Motorcycle";
+        private static final String sideCar = "";
+        private static final String TABLE_NAME5 = "Programmer";
+        private static final String nbProjects = "";
+        private static final String TABLE_NAME6 = "Manager";
+        private static final String nbClients = "";
+        private static final String TABLE_NAME7 = "Tester";
+        private static final String nbBugs= "";
 
 
 
@@ -32,18 +44,21 @@ public  class DataBaseHelper extends SQLiteOpenHelper
 
     @Override public void onCreate(SQLiteDatabase db) {
 
-        String sql = "CREATE TABLE " + TABLE_NAME + "(" +
+        String sql = "CREATE TABLE " + TABLE_NAME1 + "(" +
                 EmployeeID + " INTEGER NOT NULL CONSTRAINT employee_pk PRIMARY KEY AUTOINCREMENT, " +
-                FIRST_NAME + " varchar(200) NOT NULL, " +
-                LAST_NAME + " varchar(200) NOT NULL, " +
-                BIRTH_YEAR + " varchar(200) NOT NULL, " +
-                MONTHLY_SALARY + " double NOT NULL),"+
-                OCCUPATION_RATE  + " varchar(200) NOT NULL),"+
-                EMPLOYEE_TYPE   + " varchar(200) NOT NULL),"+
-                PROJECTS  + " varchar(200) NOT NULL),"+
-                CLIENTS   + " varchar(200) NOT NULL),"+
-                 BUGS +" varchar(200) NOT NULL),";
+                FIRST_NAME + " string(200) NOT NULL, " +
+                LAST_NAME + " string(200) NOT NULL, " +
+                BIRTH_YEAR + " int(200) NOT NULL, " +
+                MONTHLY_SALARY + " double NOT NULL,"+
+                OCCUPATION_RATE  + " int(200) NOT NULL,"+
+                EMPLOYEE_TYPE   + " string(200) NOT NULL);";
                 db.execSQL(sql);
+        String sq2="CREATE TABLE " + TABLE_NAME2 + "(" +
+                model + " int(200) NOT NULL, " +
+                plate + " String(200) NOT NULL, " +
+                color + " String(200) NOT NULL);";
+        db.execSQL(sq2);
+
 
     }
     @Override
--- app/src/main/java/com/Catalog/RegisterEmployee.java
+++ app/src/main/java/com/Catalog/RegisterEmployee.java
@@ -79,4 +79,5 @@ public class RegisterEmployee extends AppCompatActivity {
 
 
 
+
 }
