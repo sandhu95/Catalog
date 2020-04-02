ea7afb9cdf3f2d1ead2867f46acd693c75fbb797
Manpreet <sandhu.07@gamil.com>
2020-04-01 17:10:30 -0800

    added more checks to validate data in the form

--- app/src/main/java/com/Catalog/RegisterEmployee.java
+++ app/src/main/java/com/Catalog/RegisterEmployee.java
@@ -9,19 +9,19 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class RegisterEmployee extends AppCompatActivity{
 
     EditText firstName,lastName,birthYear,monthlySalary,occupationRate,employeeID,projects,clients,
     bugs,vehicleModel,plateNumber;
+    TextView clientsLabel, bugsLabel, projectsLabel, sideCarLabel,carTypeLabel;
     Spinner  employeeType,carTypeSpinner,vehicleColorSpinner;
-    RadioGroup vehicalTypeRadioGroup,sideCarRadioGroup;
+    RadioGroup vehicleTypeRadioGroup,sideCarRadioGroup;
     RadioButton  carRadioBtn,motorcycleRadioBtn,sidecarYesRadioBtn,sidecarNoRadioBtn;
-    Button rgstrbtn;
+    Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +34,16 @@ public class RegisterEmployee extends AppCompatActivity{
         occupationRate  = findViewById(R.id.occupationRateText);
         employeeID  = findViewById(R.id.employeeIDText);
         employeeType = findViewById(R.id.employeeTypeSpinner);
+        projectsLabel = findViewById(R.id.projectsLabel);
         projects = findViewById(R.id.projectsText);
+        clientsLabel = findViewById(R.id.clientsLabel);
         clients = findViewById(R.id.clientsText);
+        bugsLabel = findViewById(R.id.bugsLabel);
         bugs = findViewById(R.id.bugsText);
-        vehicalTypeRadioGroup=findViewById(R.id.vehicleTypeRadioGroup);
+        vehicleTypeRadioGroup=findViewById(R.id.vehicleTypeRadioGroup);
+        sideCarLabel = findViewById(R.id.SideCarLabel);
         sideCarRadioGroup=findViewById(R.id.SideCarRadioGroup);
+        carTypeLabel = findViewById(R.id.carTypeLabel);
         carRadioBtn = findViewById(R.id.carRadioButton);
         motorcycleRadioBtn = findViewById(R.id.motorcycleRadioButton);
         sidecarYesRadioBtn = findViewById(R.id.sidecarYesRadioButton);
@@ -47,17 +52,18 @@ public class RegisterEmployee extends AppCompatActivity{
         vehicleModel = findViewById(R.id.vehicleModelText);
         plateNumber = findViewById(R.id.plateNumberText);
         vehicleColorSpinner = findViewById(R.id.vehicleColorSpinner);
-        rgstrbtn = findViewById(R.id.registerButton);
+        registerButton = findViewById(R.id.registerButton);
 
-        rgstrbtn.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addEmployee();
+                addEmployee(v);
             }
         });
+
     }
 
-    private void addEmployee(){
+    private void addEmployee(View view){
         String fName ="";
         if(!firstName.getText().toString().isEmpty())
         fName = firstName.getText().toString().trim();
@@ -113,46 +119,111 @@ public class RegisterEmployee extends AppCompatActivity{
             employeeType.requestFocus();
             return;
         }
-        String eType = employeeType.getSelectedItem().toString();
-            if(eType.equals("Manager"))
-            {
-                //let us suppose each manager travels for 24 days in a month
-                int gainFactorClient=500, gainFactorTravel=100, numberofDaysTravelled=24*12;
-                int numberofclients=Integer.valueOf(clients.getText().toString());
-                aSalary = aSalary + ( gainFactorClient* numberofclients) +  gainFactorTravel*numberofDaysTravelled;
-            }
-            if(eType.equals("Tester"))
-            {
-                int gainFactorError = 10;
-                int numberofbugs=Integer.valueOf(clients.getText().toString());
-                aSalary = aSalary + gainFactorError * numberofbugs;
-            }
-            if(eType.equals("Programmer"))
-            {
-                int gainFactorProjects = 200;
-                int numberofprojects=Integer.valueOf(projects.getText().toString());
-                aSalary = aSalary + gainFactorProjects * numberofprojects;
-            }
 
-        int empdId = Integer.valueOf(employeeID.getText().toString());
+        String eType = employeeType.getSelectedItem().toString();
+        if(eType.equals("Manager")){
+            clientsLabel.setVisibility(view.VISIBLE);
+            clients.setVisibility(view.VISIBLE);
+        }
+        if(eType.equals("Tester"))
+        {
+            bugsLabel.setVisibility(view.VISIBLE);
+            bugs.setVisibility(view.VISIBLE);
+        }
+        if(eType.equals("Programmer")) {
+            projectsLabel.setVisibility(view.VISIBLE);
+            projects.setVisibility(view.VISIBLE);
+        }
+        if(eType.equals("Manager"))
+        {
+            //let us suppose each manager travels for 24 days in a month
+            int gainFactorClient=500, gainFactorTravel=100, numberofDaysTravelled=24*12;
+            if(clients.getText().toString().isEmpty()){
+                clients.setError("Enter clients made!");
+                clients.requestFocus();
+                return;
+            }
+            int numberofclients=Integer.valueOf(clients.getText().toString());
+            aSalary = aSalary + ( gainFactorClient* numberofclients) +  gainFactorTravel*numberofDaysTravelled;
+
+        }
+        if(eType.equals("Tester"))
+        {
+            int gainFactorError = 10;
+            if(bugs.getText().toString().isEmpty()){
+                bugs.setError("Enter bugs solved!");
+                bugs.requestFocus();
+                return;
+            }
+            int numberofbugs=Integer.valueOf(bugs.getText().toString());
+            aSalary = aSalary + gainFactorError * numberofbugs;
+        }
+        if(eType.equals("Programmer")) {
+            int gainFactorProjects = 200;
+            if(projects.getText().toString().isEmpty()){
+                projects.setError("Enter projects Completed!");
+                projects.requestFocus();
+                return;
+            }
+            int numberofprojects=Integer.valueOf(projects.getText().toString());
+            aSalary = aSalary + gainFactorProjects * numberofprojects;
+        }
+
+        if(!carRadioBtn.isChecked()&&!motorcycleRadioBtn.isChecked()){
+            Toast.makeText(this, "choose vehicle owned", Toast.LENGTH_SHORT).show();
+            vehicleTypeRadioGroup.requestFocus();
+            return;
+        }
         if(carRadioBtn.isChecked())
          {
+             carTypeLabel.setVisibility(view.VISIBLE);
+             carTypeSpinner.setVisibility(view.VISIBLE);
+             if (carTypeSpinner.getSelectedItem().toString().equals("Choose Type"))
+             {
+                 Toast.makeText(this, "choose Car Type", Toast.LENGTH_SHORT).show();
+                 carTypeSpinner.requestFocus();
+                 return;
+             }
             String cType = carTypeSpinner.getSelectedItem().toString();
          }
+        String sidecar="";
         if(motorcycleRadioBtn.isChecked())
          {
+             if(!sidecarNoRadioBtn.isChecked()&&!sidecarYesRadioBtn.isChecked()){
+                 Toast.makeText(this, "select Yes or No for sidecar!", Toast.LENGTH_SHORT).show();
+                 sideCarRadioGroup.requestFocus();
+                 return;
+             }
              if(sidecarNoRadioBtn.isChecked())
                 {
                     //Employee has motorcycle without a Side Car
+                    sidecar="No";
                 }
              if(sidecarYesRadioBtn.isChecked())
                 {
                     //Employee has motorcycle with a Side Car
+                    sidecar = "Yes";
                 }
          }
-
+        if(vehicleModel.getText().toString().isEmpty()){
+            vehicleModel.setError("Enter make year!");
+            vehicleModel.requestFocus();
+            return;
+        }
         int vModel= Integer.valueOf(vehicleModel.getText().toString());
+
+        if(plateNumber.getText().toString().isEmpty()){
+            plateNumber.setError("enter vehicle plate!");
+            plateNumber.requestFocus();
+            return;
+        }
         String pNumber = plateNumber.getText().toString().trim();
+
+        if (vehicleColorSpinner.getSelectedItem().toString().equals("Choose a Color")) {
+            Toast.makeText(this, "choose color of Vehicle", Toast.LENGTH_SHORT).show();
+            vehicleColorSpinner.requestFocus();
+            return;
+        }
         String vColor = vehicleColorSpinner.getSelectedItem().toString().trim();
 
     }
