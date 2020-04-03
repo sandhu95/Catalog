de596a613da4e86c3d21f0b7351b91218b66e1c5
Ramanpreet Kaur <ramanpreetkaor@gmail.com>
2020-04-02 20:02:27 -0400

    Added list in mainActivity.xml

--- app/src/main/java/com/Catalog/MainActivity.java
+++ app/src/main/java/com/Catalog/MainActivity.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class
+MainActivity extends AppCompatActivity
+{
+    private static final String TAG = "EmployeeActivity";
+    DataBaseHelper Database;
+    List<Employee> employeeList;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listview);
+        employeeList = new ArrayList<>();
+        Database = new DataBaseHelper(this);
+
     }
     public void addEmployee(View view){
         Intent intent = new Intent(this, RegisterEmployee.class);
